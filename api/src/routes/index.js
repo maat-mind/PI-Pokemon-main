c19402ad502dfa71a0554f7eb0fce754a795ba27
Oscar Bernal <odbg2013@gmail.com>
2022-09-26 16:21:15 -0500

    update PI-Pokemon-main

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,9 @@
+const axios = require('axios')
 const { Router } = require('express')
 const { Pokemon, Type } = require('../db.js')
-const axios = require('axios')
+const { getAllPokemons } = require('../controllers')
 
 const router = Router()
-
 
 router.get('/pokemons', async (req, res) => {
   try {
@@ -38,15 +38,21 @@ router.put('/pokemons', async (req, res) => {
 
 router.get('/types', async (req, res) => {
   try {
-    const types = await Type.findAll()
-    res.json(types)
+    const api = await axios.get('https://pokeapi.co/api/v2/type')
+    const apiTypes = api.data.results.map((e) => e.name)
+
+    apiTypes.forEach((e) => {
+      Type.findOrCreate({
+        where: { name: e },
+      })
+    })
+
+    const pokemonTypes = await Type.findAll()
+    res.send(pokemonTypes)
+    
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
 })
-
-// router.get('pokemons/:id', async (req, res) => {})
-
-// router.get('pokemons/:name', async (req, res) => {})
 
 module.exports = router
