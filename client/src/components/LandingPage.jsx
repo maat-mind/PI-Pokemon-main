7f52b22c8a662ded0413c4967665a8f6e129f1c2
Oscar Bernal <odbg2013@gmail.com>
2022-09-27 17:14:36 -0500

    landing page image
    added image, fonts and css to the landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './LandingPage.css'
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className='container'>
       <h1>Bienvenido Entrenador</h1>
       <Link to='/home'>
         <button>Ingresar</button>
