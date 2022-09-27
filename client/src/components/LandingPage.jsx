60e63960a3c81c66ee62fba2f31e8a17fefa8d35
Oscar Bernal <odbg2013@gmail.com>
2022-09-27 18:47:30 -0500

    landing title and button
    completed css of landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,10 +5,13 @@ import './LandingPage.css'
 export default function LandingPage() {
   return (
     <div className='container'>
-      <h1>Bienvenido Entrenador</h1>
+      <span className='pokedex-title'></span>
       <Link to='/home'>
-        <button>Ingresar</button>
+        <button>
+          <span className='focus-option'> ▶</span> Ingresar
+        </button>
       </Link>
+      <h2>con cariño maat-mind 💛</h2>
     </div>
   )
 }
