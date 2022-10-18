7a96c4b773c74bcd8338f45e27ec07f7213114c6
Oscar Bernal <odbg2013@gmail.com>
2022-10-18 17:03:26 -0500

    icons on pokemon detail

--- client/src/components/DetailPokemon/DetailPokemon.jsx
+++ client/src/components/DetailPokemon/DetailPokemon.jsx
@@ -10,7 +10,7 @@ import hpIcon from '../../assets/icons/hp.png'
 import speedIcon from '../../assets/icons/speed.png'
 import weightIcon from '../../assets/icons/weight.png'
 
-// import style from './DetailPokemon.module.css'
+import style from './DetailPokemon.module.css'
 
 const DetailPokemon = () => {
   const { id } = useParams()
@@ -30,15 +30,37 @@ const DetailPokemon = () => {
         <button>◃</button>
       </Link>
 
-      <h1>{p.name}</h1>
-      <img src={p.img} alt={p.name} />
-      <p> {p.hp}</p>
-      <p> {p.attack}</p>
-      <p> {p.defense}</p>
-      <p> {p.weight}</p>
-      <p> {p.height}</p>
-      <p> {p.speed}</p>
-      <p> {p.types}</p>
+      <h1 className={style.pokemonName}>{p.name}</h1>
+      <img className={style.pokemonImage} src={p.img} alt={p.name} />
+      <p>
+        <img className={style.statIcon} src={hpIcon} alt='hp icon' />
+        {p.hp}
+      </p>
+      <p>
+        <img className={style.statIcon} src={attackIcon} alt='attack icon' />
+        {p.attack}
+      </p>
+      <p>
+        <img className={style.statIcon} src={defenseIcon} alt='defense icon' />
+        {p.defense}
+      </p>
+      <p>
+        <img className={style.statIcon} src={speedIcon} alt='speed icon' />
+        {p.speed}
+      </p>
+      <p>
+        <img className={style.statIcon} src={weightIcon} alt='weight icon' />
+        {p.weight} hg
+      </p>
+      <p>
+        <img className={style.statIcon} src={heightIcon} alt='height icon' />
+        {p.height} dm
+      </p>
+      <p>
+        {p.types.map((e) => {
+          return <span className={`${style.type} ${e}`}>{e}</span>
+        })}
+      </p>
     </>
   )
 }
