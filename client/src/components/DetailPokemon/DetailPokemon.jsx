287a7773be284dcd40dce21a45907cb7bf952401
Oscar Bernal <odbg2013@gmail.com>
2022-10-20 09:36:10 -0500

    detail pokemon update style

--- client/src/components/DetailPokemon/DetailPokemon.jsx
+++ client/src/components/DetailPokemon/DetailPokemon.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
-import { getDetail } from '../../redux/actions'
+import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Link, useParams} from 'react-router-dom'
+import {getDetail} from '../../redux/actions'
 
 import attackIcon from '../../assets/icons/attack.png'
 import defenseIcon from '../../assets/icons/defense.png'
@@ -14,7 +14,7 @@ import loading from '../../assets/img/loading.gif'
 import style from './DetailPokemon.module.css'
 
 const DetailPokemon = () => {
-  const { id } = useParams()
+  const {id} = useParams()
 
   const dispatch = useDispatch()
   const pokemon = useSelector((state) => state.pokemonDetail)
@@ -25,53 +25,58 @@ const DetailPokemon = () => {
 
   if (Array.isArray(pokemon)) {
     return (
-      <>
-        <p>Estoy cargando...</p>
-        <img src={loading} alt='loading gif' />
-      </>
+      <main className={style.mainDetail}>
+        <img src={loading} alt="loading gif"/>
+      </main>
     )
   } else {
     return (
-      <>
-        <Link to='/home'>
+      <main className={style.mainDetail}>
+        <Link to="/home">
           <button>◃</button>
         </Link>
 
-        <h1 className={style.pokemonName}>{pokemon.name}</h1>
-        <img
-          className={style.pokemonImage}
-          src={pokemon.img}
-          alt={pokemon.name}
-        />
-        <p>
-          <img className={style.statIcon} src={hpIcon} alt='hp icon' />
-          {pokemon.hp}
-        </p>
-        <p>
-          <img className={style.statIcon} src={attackIcon} alt='attack icon' />
-          {pokemon.attack}
-        </p>
-        <p>
+        <div className={style.cardDetail}>
+          <h1 className={style.pokemonName}>{pokemon.name}</h1>
           <img
-            className={style.statIcon}
-            src={defenseIcon}
-            alt='defense icon'
+            className={style.pokemonImage}
+            src={pokemon.img}
+            alt={pokemon.name}
           />
-          {pokemon.defense}
-        </p>
-        <p>
-          <img className={style.statIcon} src={speedIcon} alt='speed icon' />
-          {pokemon.speed}
-        </p>
-        <p>
-          <img className={style.statIcon} src={weightIcon} alt='weight icon' />
-          {pokemon.weight} hg
-        </p>
-        <p>
-          <img className={style.statIcon} src={heightIcon} alt='height icon' />
-          {pokemon.height} dm
-        </p>
-      </>
+          <div className={style.cardStats}>
+
+            <p className={style.textStat}>
+              <img className={style.statIcon} src={hpIcon} alt="hp icon"/>
+              {pokemon.hp}
+            </p>
+            <p className={style.textStat}>
+              <img className={style.statIcon} src={attackIcon} alt="attack icon"/>
+              {pokemon.attack}
+            </p>
+            <p className={style.textStat}>
+              <img
+                className={style.statIcon}
+                src={defenseIcon}
+                alt="defense icon"
+              />
+              {pokemon.defense}
+            </p>
+            <p className={style.textStat}>
+              <img className={style.statIcon} src={speedIcon} alt="speed icon"/>
+              {pokemon.speed}
+            </p>
+            <p className={style.textStat}>
+              <img className={style.statIcon} src={weightIcon} alt="weight icon"/>
+              {pokemon.weight} hg
+            </p>
+            <p className={style.textStat}>
+              <img className={style.statIcon} src={heightIcon} alt="height icon"/>
+              {pokemon.height} dm
+            </p>
+          </div>
+        </div>
+
+      </main>
     )
   }
 }
